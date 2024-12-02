85a707fb0ff18a21cf6db87d489f8dab3037e9d5
belleanda <philliplife.ag@gmail.com>
2024-12-02 14:57:53 +0700

    Add files via upload

---
+++ ณัฐริณีย์33.py
@@ -0,0 +1,9 @@
+province=["กรุงเทพมหานคร","กาฬสินธ์ุ","กาญจนบุรี","จันทบุรี","ฉะเชิงเทรา","ชลบุรี","พะเยา","ชุมพร","ชัยนาท","ข่อนแก่น","ยโสธร","นครพนม","นครราชสีมา","เชียงใหม่"]
+
+province[0]="Bangkok"
+province[12]="Korat"
+province[4]="Peat Riw"
+province[5]="Chon buri"
+province[7]="Chum porn"
+
+print(province)
